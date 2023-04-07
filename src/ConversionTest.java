ce3ab38359a031f0147c39b0aae5dc0cb2e5a861
RyanZurrin <ryanzurrin@ryanzurrin.com>
2023-04-07 03:11:29 -0400

    Fixed some bugs that were causing the wrong conversions from boolean to other types which were being classified as Widening when they should of been errors.

--- src/ConversionTest.java
+++ src/ConversionTest.java
@@ -1,8 +1,22 @@
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ConversionTest {
     static String error = "Error";
+    private static final Map<Class<?>, Class<?>> PRIMITIVE_TO_WRAPPER_MAP = Map.of(
+            boolean.class, Boolean.class,
+            byte.class, Byte.class,
+            char.class, Character.class,
+            short.class, Short.class,
+            int.class, Integer.class,
+            long.class, Long.class,
+            float.class, Float.class,
+            double.class, Double.class
+    );
+
+    private static final Map<Class<?>, Class<?>> WRAPPER_TO_PRIMITIVE_MAP = PRIMITIVE_TO_WRAPPER_MAP.entrySet().stream()
+            .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
     public static String checkConversion(Class<?> from, Class<?> to) {
         if (from.equals(to)) {
             return "Identity";
@@ -13,36 +27,52 @@ public class ConversionTest {
         }
 
         if (from.isPrimitive()) {
-            return checkBoxingConversion(from, to);
+            Class<?> boxedFrom = box(from);
+            if (boxedFrom.equals(to)) {
+                return "Boxing";
+            }
+            if (to.isAssignableFrom(boxedFrom)) {
+                return "Boxing & Widening";
+            }
         }
 
         if (to.isPrimitive()) {
-            return checkUnboxingConversion(from, to);
+            Class<?> unboxedFrom = unbox(from);
+            if (unboxedFrom.equals(to)) {
+                return "Unboxing";
+            }
+            if (unboxedFrom.isAssignableFrom(to)) {
+                return "Unboxing & Widening";
+            }
+            if (to.isAssignableFrom(unboxedFrom)) {
+                return "Unboxing & Narrowing";
+            }
         }
 
         if (to.isAssignableFrom(from)) {
             return "Widening";
         }
 
-
         return error;
     }
 
     private static String checkPrimitiveConversion(Class<?> from, Class<?> to) {
-        // List of primitive types in widening order
-        List<Class<?>> wideningOrder = List.of(
-                byte.class, short.class, int.class, long.class, float.class, double.class
+        // List of primitive types in widening order, including char
+        List<Class<?>> numericWideningOrder = List.of(
+                byte.class, short.class, char.class, int.class, long.class, float.class, double.class
         );
 
-        int fromIndex = wideningOrder.indexOf(from);
-        int toIndex = wideningOrder.indexOf(to);
+        int fromIndex = numericWideningOrder.indexOf(from);
+        int toIndex = numericWideningOrder.indexOf(to);
 
-        if (fromIndex < toIndex) {
-            return "Widening";
-        }
+        if (fromIndex != -1 && toIndex != -1) {
+            if (fromIndex < toIndex) {
+                return "Widening";
+            }
 
-        if (fromIndex > toIndex) {
-            return "Narrowing";
+            if (fromIndex > toIndex) {
+                return "Narrowing";
+            }
         }
 
         return error;
@@ -87,17 +117,28 @@ public class ConversionTest {
         return error;
     }
 
+    private static Class<?> box(Class<?> primitive) {
+        return PRIMITIVE_TO_WRAPPER_MAP.get(primitive);
+    }
+
+    private static Class<?> unbox(Class<?> wrapper) {
+        return WRAPPER_TO_PRIMITIVE_MAP.get(wrapper);
+    }
+
     // main method for testing
     public static void main(String[] args) {
-        System.out.println("To: \t" + "bool\t" + "byte\t" + "char\t" + "short\t" + "int\t" + "long\t" + "float\t" + "double\t" + "Byte\t" + "Short\t" + "Boolean\t" + "Character\t" + "Integer\t" + "Long\t" + "Float\t" + "Double\t");
+        System.out.println("To: \t" + "bool\t" + "byte\t" + "char\t" + "short\t" + "int\t" + "long\t" + "float\t" +
+                "double\t" + "Boolean\t" + "Byte\t" + "Character\t" + "Short\t" + "Integer\t" + "Long\t" +
+                "Float\t" + "Double\t");
         System.out.println("From:");
         // define the classes to be used in the table
 
 
         Class<?>[] classes;
         // define the classes to be used in the table
-        classes = new Class<?>[]{boolean.class, byte.class, char.class, short.class, int.class, long.class, float.class, double.class,
-                Boolean.class, Byte.class, Character.class, Short.class, Integer.class, Long.class, Float.class, Double.class};
+        classes = new Class<?>[]{boolean.class, byte.class, char.class, short.class, int.class, long.class,
+                float.class, double.class, Boolean.class, Byte.class, Character.class, Short.class, Integer.class,
+                Long.class, Float.class, Double.class};
         // print the table
         for (Class<?> from : classes) {
             System.out.print(from.getSimpleName() + "\t");
